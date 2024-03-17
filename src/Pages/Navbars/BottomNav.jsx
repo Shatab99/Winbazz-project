6efa91aa3f86f427a7213bc9d483ae64acc5b669
shatab <shatabag4749@gmail.com>
2024-03-17 16:47:23 +0600

    Added bottom nav slidly edited

--- src/Pages/Navbars/BottomNav.jsx
+++ src/Pages/Navbars/BottomNav.jsx
@@ -10,26 +10,26 @@ const BottomNav = () => {
 
 
         <div className="w-full lg:hidden">
-            <section id="bottom-navigation" className="block fixed inset-x-0 bottom-0 z-10 bg-orange-100 shadow ">
+            <section id="bottom-navigation" className="block fixed inset-x-0 bottom-0 z-10 bg-orange-100 shadow px-2">
                 <div id="tabs" className="flex justify-between">
                     <Link href="#" className="w-full focus:text-orange-500 hover:text-orange-500 flex items-center flex-col text-center pt-2 pb-1">
-                        <FaRegMessage className="text-5xl" />
+                        <FaRegMessage className="text-3xl" />
                         <span className="tab tab-home block text-xs">Messages</span>
                     </Link>
                     <Link href="#" className="w-full focus:text-orange-500 hover:text-orange-500 flex items-center flex-col text-center pt-2 pb-1">
-                        <GiWallet className="text-5xl" />
+                        <GiWallet className="text-3xl" />
                         <span className="tab tab-kategori block text-xs">Deposite</span>
                     </Link>
                     <Link href="#" className="w-full focus:text-orange-500 hover:text-orange-500 flex items-center flex-col text-center pt-2 pb-1">
-                        <IoHome className="text-5xl" />
+                        <IoHome className="text-3xl" />
                         <span className="tab tab-explore block text-xs">Home</span>
                     </Link>
                     <Link href="#" className="w-full focus:text-orange-500 hover:text-orange-500 flex items-center flex-col text-center pt-2 pb-1">
-                        <BiMoneyWithdraw className="text-5xl" />
+                        <BiMoneyWithdraw className="text-3xl" />
                         <span className="tab tab-explore block text-xs">Withdraw</span>
                     </Link>
                     <Link href="#" className="w-full focus:text-orange-500 hover:text-orange-500 flex items-center flex-col text-center pt-2 pb-1">
-                        <FaUser className="text-5xl" />
+                        <FaUser className="text-3xl" />
                         <span className="tab tab-explore block text-xs">Profile</span>
                     </Link>
 
