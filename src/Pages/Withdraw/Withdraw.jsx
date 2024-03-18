7c6e5118e87072e89a67e882e5251776bc0ba8f2
shatab <shatabag4749@gmail.com>
2024-03-18 13:49:46 +0600

    designed withdraw page

--- src/Pages/Withdraw/Withdraw.jsx
+++ src/Pages/Withdraw/Withdraw.jsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
 import { IoReload } from "react-icons/io5";
+import bikash from '../../assets/Icons/BKash-Icon-Logo.wine.svg'
+import nagad from '../../assets/Icons/Nagad-Logo.wine.png'
+import upay from '../../assets/Icons/upay-icon.png'
+import rocket from '../../assets/Icons/rocket-logo.png'
+import WithdrawModal from "./WithdrawModal";
 
 
 const Withdraw = () => {
 
     const balance = 980;
     const [amount, setAmount] = useState(0)
+    const [method, setMethod] = useState(null)
+    const [phone, setPhone] = useState(null)
     const [err, setErr] = useState('')
+    const [isOpen, setIsOpen]= useState(false)
 
     useEffect(() => {
         if (amount === 0) {
             setErr('')
         }
-        else if (amount < 500) {
-            setErr('Minimum 500 BDT')
+        else if (amount < 800) {
+            setErr('Minimum 800 BDT')
         }
         else if (amount > balance) {
             setErr('Not Enought Balance')
@@ -38,17 +46,22 @@ const Withdraw = () => {
             </div>
             <div className="flex flex-col items-start max-w-xs justify-center mx-auto gap-5">
                 <p>Select Amount <span className="text-red-600">*</span></p>
-                <input onChange={(e) => setAmount(e.target.value)} type="number" placeholder="Minimum 500 BDT - Maximum 30000 BDT " className="input input-bordered w-full max-w-xs" />
+                <input onChange={(e) => setAmount(e.target.value)} type="number" placeholder="Minimum 800 BDT - Maximum 30000 BDT " className="input input-bordered w-full max-w-xs" />
                 <p className="text-red-700">{err}</p>
                 <p>Select Payment Method <span className="text-red-600">*</span></p>
-                <select className="select select-bordered w-full max-w-xs">
-                    <option>Bkash</option>
-                    <option>Nagad</option>
-                    <option>Upay</option>
-                    <option>Rocket</option>
-                </select>
-                <button disabled={!amount || amount < 500 || amount > balance} className="btn bg-blue-700 text-white w-full ">Withdraw</button>
+                <div className='flex flex-wrap items-center justify-start mt-4 gap-2'>
+                    <img onClick={() => setMethod('Bkash')} src={bikash} alt="" className={`w-24 h-20 border-2 rounded-2xl ${method === 'Bkash' && 'border-orange-400'} rounded-xl`} />
+                    <img onClick={() => setMethod('Nagad')} src={nagad} alt="" className={`w-24 h-20 border-2 rounded-2xl ${method === 'Nagad' && 'border-orange-400'} rounded-xl`} />
+                    <img onClick={() => setMethod('Upay')} src={upay} alt="" className={`w-24 h-20 border-2 rounded-2xl ${method === 'Upay' && 'border-orange-400'} rounded-xl`} />
+                    <img onClick={() => setMethod('Rocket')} src={rocket} alt="" className={`w-24 h-20 border-2 rounded-2xl ${method === 'Rocket' && 'border-orange-400'} rounded-xl`} />
+                </div>
+                <div className="w-full mb-3">
+                    <p className="mb-4">Enter Phone Number <span className="text-red-600">*</span></p>
+                    <input onChange={(e)=> setPhone(e.target.value)} type="number" placeholder="Enter your phone Number " className="input input-bordered w-full max-w-sm" />
+                </div>
+                <button onClick={()=>setIsOpen(true)} disabled={!amount || amount < 800 || amount > balance || !method || phone?.length <11 | !phone } className="btn bg-blue-700 text-white w-full ">Withdraw</button>
             </div>
+            <WithdrawModal isOpen={isOpen} setIsOpen={setIsOpen} method={method} amount={amount} />
         </div>
     );
 };
